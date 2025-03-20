b902acf54729e1d18f078852190213a7f7ad95fc
kaveenhyacinth <alwiskaveen@gmail.com>
2025-03-20 06:42:56 +0530

    fix: update footer to display the current year dynamically

--- client/src/components/atoms/footer.tsx
+++ client/src/components/atoms/footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="w-full h-10 flex flex-col justify-end">
       <p className="font-montserrat text-sm text-center md:text-left">
-        © 2023 <a href="https://kaveenhyacinth.com" target="_blank">Kaveen Hyacinth</a>. All Rights Reserved.
+        © {new Date().getFullYear()} <a href="https://kaveenhyacinth.com" target="_blank">Kaveen Hyacinth</a>. All Rights Reserved.
       </p>
     </footer>
   )
